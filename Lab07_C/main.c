dcdca33c6a2170a7bf50b7c302364cbbfb92a4da
John Hervey <jhervey@oucs1394.otago.ac.nz>
2012-08-22 14:16:22 +1200

    Initial Commit

---
+++ Lab07_C/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Lab07_C
+//
+//  Created by John Hervey on 8/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
